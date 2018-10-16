2fb6ec2efed3894a3529bf53fcb9a69bfbc6b773
annchichii <44175855+annchichii@users.noreply.github.com>
2018-10-15 21:17:05 -0700

    Add logos
    Sponsor logo updates

---
+++ diversitechy/diversitechy/js/custom.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+	// header color change
+	$(window).on("scroll",function(){
+		var scrollDistance = $(window).scrollTop();
+		var $header = $(".js-header");
+		if(scrollDistance > 80){
+			$header.addClass("header--colored");
+		}
+		else{
+			$header.removeClass("header--colored");
+		}
+	})
+
+	
+})
